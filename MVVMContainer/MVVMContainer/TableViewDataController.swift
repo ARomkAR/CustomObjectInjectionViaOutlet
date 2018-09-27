80ad7191a67068754c4b44b9c13a6895a1a026d3
Omkar khedekar <maxios.om@gmail.com>
2018-09-27 09:05:43 +0530

    Most simple example

---
+++ MVVMContainer/MVVMContainer/TableViewDataController.swift
@@ -0,0 +1,40 @@
+//
+//  TableViewDataController.swift
+//  MVVMContainer
+//
+//  Created by Omkar khedekar on 27/09/18.
+//  Copyright © 2018 Omkar khedekar. All rights reserved.
+//
+
+import UIKit
+
+class TableViewDataController: NSObject {
+
+    @IBOutlet weak var tabelView: UITableView! {
+        didSet {
+            self.tabelView.register(UITableViewCell.self, forCellReuseIdentifier: "CellIdentfier")
+        }
+    }
+}
+
+extension TableViewDataController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellIdentfier", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+}
+
+extension TableViewDataController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+}
--- MVVMContainer/MVVMContainer/ViewController.swift
+++ MVVMContainer/MVVMContainer/ViewController.swift
@@ -9,37 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var picTitle: UILabel!
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var imageDecription: UITextView!
-    @IBOutlet var viewModel: ViewModel!
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.viewModel.fetchImageOfDay { [weak self] result in
-            switch result {
-            case .success(let pic):
-                DispatchQueue.main.async {
-                    self?.imageDecription.text = pic.explanation
-                    self?.picTitle.text = "\(pic.title)\n@\(pic.date)"
-                }
-
-                guard let hdURL = URL(string: pic.hdurl) else { return }
-                DispatchQueue.global().async {
-                    do {
-                        let data =  try Data(contentsOf: hdURL)
-                        DispatchQueue.main.async {
-                            self?.imageView.image = UIImage(data: data)
-                        }
-                    } catch {
-                        print(error)
-                    }
-                }
-            case .failed:
-                print(result)
-            }
-        }
     }
 }
 
